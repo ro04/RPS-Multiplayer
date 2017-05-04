e9a68a15d072318fb0220265dfb999595cedca6b
Rochelle Beal <bealrochelle@gmail.com>
2017-05-04 08:58:37 -0500

    Added player1 and player2 text to the right/left panels after they login

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -23,33 +23,6 @@ $(document).ready(function () {
         waitingToJoin();
     });
  
-
-    //var controller = function() {
-        //createPlayer();
-        //waitingToJoin(playingState);  //>player is trying to join the game.
-    //};
-
-    var syncToFirebase = function(playerNum) {
-        //for(var i = 0; i < numPlayers; i++) {
-            gameRef.child('player' + playerNum + '/online').set({
-                Name: playerName,
-                Wins: playerWins,
-                Loses: playerLoses
-             });
-
-             if(playerNum === 0) {
-                $("#player-one-text").remove();
-                $("#user").remove();
-                $("#submit-btn").remove();
-             }else{
-                $("#player-two-text").remove();
-                $("#user").remove();
-                $("#submit-btn").remove();
-             }
-
-        }
-    //};
-
     var waitingToJoin = function() {
         // Listen on "online" location for player 0 
         gameRef.child("player0/online").on("value", function (snapshot){
@@ -84,14 +57,120 @@ $(document).ready(function () {
                 playingState = PlayingState.Playing;
                 //startPlaying(playerNum);
                 syncToFirebase(playerNum);
+                //startPlaying(playerNum);
             }else{
                 playState = PlayingState.Watching;
             }   
         }); 
     }; 
 
-    var playingGame = function(playerNum) {
-        
+    var syncToFirebase = function(playerNum) {
+        gameRef.child('player' + playerNum + '/online').set({
+            Name: playerName,
+            Wins: playerWins,
+            Loses: playerLoses
+        });
+
+         //Control the players
+        playerOneRef = gameRef.child('player0/online');
+        playerTwoRef = gameRef.child('player1/online');
+
+
+        playerOneRef.on("value", function(playerOneSnapshot){
+            if(playerOneSnapshot.val() !== null){
+                 //Initializing Variables
+                var player_one_name = playerOneSnapshot.val().Name;
+                var player_one_wins =  playerOneSnapshot.val().Wins;
+                var player_one_loses = playerOneSnapshot.val().Loses;
+                //Remove submit button, text label, and replace "waiting for player one"
+                //with the players name
+                $("#submit-btn").remove();
+                $("#user").remove();
+                $("#player-one-text").empty();
+                $("#player-one-text").append("Welcome " + player_one_name + " You Are Player 1");
+
+                //*****Create a div to display info for Player One*****//
+                //Create the div to display player one name
+                var plNameText = $("<h4>");
+                plNameText.text(player_one_name);
+                $("#p1-name-text").append(plNameText );
+
+                //Create the div to display rock text
+                var plRockText = $("<a href='#'>");
+                plRockText.text("ROCK");
+                $("#p1-rock-text").append(plRockText);
+
+                //Create the div to display paper text
+                var p1PaperText = $("<a href='#'>");
+                p1PaperText.text("PAPER");
+                $("#p1-paper-text").append(p1PaperText);
+
+                //Create the div to display scissor text
+                var p1ScissorText = $("<a href='#'>");
+                p1ScissorText.text("SCISSOR");
+                $("#p1-scissor-text").append(p1ScissorText);
+
+                //Create the div to display wins
+                var p1NumWins = $("<h4>");
+                //console.log(snapshot.child('Wins').val());
+                p1NumWins.text("Wins: " + player_one_wins);
+                $("#p1-wins-text").append(p1NumWins);
+    
+                //Create the div to display loses
+                var p1NumLoses = $("<h4>");
+                //console.log(snapshot.child('Loses').val());
+                p1NumLoses.text("Loses: " + player_one_loses);
+                $("#p1-loses-text").append(p1NumLoses);
+            }
+           
+        })
+           
+
+        playerTwoRef.on("value", function(playerTwoSnapshot){
+            if(playerTwoSnapshot.val() !== null){
+                var player_two_name = playerTwoSnapshot.val().Name;
+                var player_two_wins = playerTwoSnapshot.val().Wins;
+                var player_two_loses = playerTwoSnapshot.val().Loses;
+                $("#submit-btn").remove(); 
+                $("#user").remove();  
+                $("#player-two-text").empty(); 
+                $("#player-two-text").append("Welcome " + player_two_name + " You Are Player 2"); 
+                //*****Create the div to display info for Player One*****//
+                //Player Two Name
+                var p2NameText = $("<h4>");
+                //console.log(snapshot.child('Name').val());
+                p2NameText.text(player_two_name);
+                $("#p2-name-text").append(p2NameText);
+                //Rock Element
+                var p2RockText = $("<a href='#'>");
+                p2RockText.text("ROCK");
+                $("#p2-rock-text").append(p2RockText);
+                //Paper Element
+                var p2PaperText = $("<a href='#'>");
+                p2PaperText.text("PAPER");
+                $("#p2-paper-text").append(p2PaperText);
+                //Scissor Element
+                var p2ScissorText = $("<a href='#'>");
+                p2ScissorText.text("SCISSOR");
+                $("#p2-scissor-text").append(p2ScissorText);
+                //Number of Wins Element
+                var p2NumWins = $("<h4>");
+                p2NumWins.text("Wins: " + player_two_wins);
+                $("#p2-wins-text").append(p2NumWins);
+                //Number of Loses Element
+                var p2NumLoses = $("<h4>");
+                p2NumLoses.text("Loses: " + player_two_loses);
+                $("#p2-loses-text").append(p2NumLoses);
+            }
+        })
+
+        startPlaying(playerOneRef, playerTwoRef);
+
+    };
+
+    var startPlaying = function(playerOneRef, playerTwoRef) {
+       this.playerOneRef = playerOneRef;
+       this.playerTwoRef = playerTwoRef;
 
     }
   
